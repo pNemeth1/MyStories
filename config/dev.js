575be475cde740bf6c586973c16978dce0853a28
Patrick Nemeth <33420664+pNemeth1@users.noreply.github.com>
2020-10-27 14:21:00 +0100

    Delete dev.js

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleClientID: '255868854837-paug9b3cuokr9lvh2go193s2uc1g2m0b.apps.googleusercontent.com',
-    googleClientSecret: '6SFc_dXjNiLfEEX26odEBe8z',
-    mongoURI: 'mongodb+srv://MadPad:QeVra8LSJxgy42PO@emaily.av0ta.mongodb.net/emailyUsers?retryWrites=true&w=majority',
-    cookieKey: 'gdhskhdde'
-}
